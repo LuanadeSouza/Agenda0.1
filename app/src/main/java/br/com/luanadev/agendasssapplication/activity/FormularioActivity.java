a4850d6ef62e5410f8e1da5018914b2c8359461e
LuanadeSouza <you@example.com>
2020-05-13 09:31:30 -0300

    usando camera

---
+++ app/src/main/java/br/com/luanadev/agendasssapplication/activity/FormularioActivity.java
@@ -0,0 +1,97 @@
+package br.com.luanadev.agendasssapplication.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+
+import br.com.luanadev.agendasssapplication.R;
+import br.com.luanadev.agendasssapplication.dao.AlunoDAO;
+import br.com.luanadev.agendasssapplication.helper.FormularioHelper;
+import br.com.luanadev.agendasssapplication.model.Aluno;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
+    private Aluno aluno;
+    public static final int CODIGO_CAMERA = 567;
+    private String caminhoFoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        helper = new FormularioHelper(this);
+
+        Intent intent = getIntent();
+        intent.getSerializableExtra("aluno");
+        if (aluno != null) {
+            helper.preencheFormulario(aluno);
+        }
+
+        Button botaoFoto = findViewById(R.id.formulario_botao_foto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                startActivityForResult(intentCamera, CODIGO_CAMERA);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CODIGO_CAMERA) {
+                helper.carregaImagem(caminhoFoto);
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                 aluno = helper.pegaAluno();
+
+                AlunoDAO dao = new AlunoDAO(this);
+                if (aluno.getId() != null) {
+                    dao.altera(aluno);
+                } else {
+                    dao.insere(aluno);
+                }
+                dao.close();
+
+                Toast.makeText(FormularioActivity.this, "Aluno " + aluno.getNome() + " salvo!", Toast.LENGTH_SHORT).show();
+
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
